6be069feb85f3016b7db5da633f62cc7cd9566b5
locustbea <locostbea@yahoo.com>
2023-07-19 14:46:19 -0700

    Debugging

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - run an infinite loop
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/*while (i < 10)*/
+	/*{*/
+	/* putchar(i);*/
+	/*}*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
