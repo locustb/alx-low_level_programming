78bb6f73c83c520acf5505404d0c20672e3e4f13
locustbea <locostbea@yahoo.com>
2023-07-21 13:47:24 -0700

    Debbuging

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- *  * print_numbers - check the code for Betty School students.
- *   * Return: zero
- *    */
+* print_numbers - check the code for Betty School students.
+* Return: zero
+*/
 void print_numbers(void)
 {
 		int a;
 
-			for (a = 0; a <= 9; a++)
-					{
-								_putchar('0' + a);
-									}
-				_putchar('\n');
+		for (a = 0; a <= 9; a++)
+		{
+		_putchar('0' + a);
+		}
+		_putchar('\n');
 }
