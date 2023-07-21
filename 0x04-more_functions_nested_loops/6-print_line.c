e7caa2ac2454b207fd22b95fbb32519fe232469f
locustbea <locostbea@yahoo.com>
2023-07-21 00:31:45 -0700

    more function nexted loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  * print_line - prints a line
+ *   * @n: number of times to print character _.
+ *    */
+
+void print_line(int n)
+{
+		int i;
+
+			for (i = 0; i < n; i++)
+					{
+								_putchar(95);
+									}
+				_putchar('\n');
+}
