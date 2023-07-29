a6c16da7c2358c5bf5cc5a6b87f489d1fe2b982c
locustbea <locostbea@yahoo.com>
2023-07-29 06:23:32 -0700

    Debbuging

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -1,14 +1,15 @@
-#include <unistd.h>
-#include "main.h"
+#include "main.h"                                                                                                                  
+#include <unistd.h>                                                                                                                     
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: 1 (Success),
- * On error, -1 is return and error is set
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
+/**                                                                                                                                     
+ * _putchar - writes the character c to stdout                                                                                          
+ * @c: The character to print                                                                                                           
+ *                                                                                                                                      
+ * Return: On success 1.                                                                                                                
+ * On error, -1 is returned, and errno is set appropriately.                                                                            
+ */                                                                                                                                     
+int _putchar(char c)                                                                                                                    
+{                                                                                                                                       
+	return (write(1, &c, 1));                                                                                                       
 }
+
