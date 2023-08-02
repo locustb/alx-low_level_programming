125dae6247df15fc6cb0f5be173ddc9525ef2ff2
locustbea <locostbea@yahoo.com>
2023-08-02 04:33:02 -0700

    Recursion

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int factorial (int n)
+{
+	int res;
+	int i;
+
+	res = 1;
+	i = 1;
+	{
+	res = res * i;
+	i++;
+	}
+	return (res);
+}
+
+int main (void)
+{
+	int f;
+	
+	f = factorial (5) ;
+	printf ("5! = %d\n", f) ;
+	return (0);
+}
