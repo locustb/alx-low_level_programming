271ee37165332c7739c7b66bf44643f93a03202f
locustbea <locostbea@yahoo.com>
2023-08-16 22:55:33 -0700

    Variadic functions

---
+++ 0x08-recursion/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: returns o
+ */
+
+int main (void)
+{
+	int age =38;
+	printf("Olalekan is %d years old \n" ,age );
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Low level programming
+Variadic functions
