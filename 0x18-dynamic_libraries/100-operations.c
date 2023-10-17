bb25c736bcb011c24eb13632015d93974bd1fd0e
locustbea <locostbea@yahoo.com>
2023-10-17 12:06:31 -0700

    0x18-dynamic_libraries

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int add(int a, int b) {
+	return a + b;
+}
+
+int mul(int a, int b) {
+	if (b == o) {
+		printf ("Error: Division by zero\n");
+		return o;
+
+	}
+	{	return a / b;
+
+	}
+
+	int mod(int a, int b){
+		if (b == o) {
+			prinf(Error: Division by zero\n");
+			return o;
+			)
+				return a % b;
