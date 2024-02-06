856bcf45da1a2bc9c35ea8f1e6d32cdfe956a2a6
locustbea <locustbea@yahoo.com>
2024-02-06 05:26:06 -0800

    advanced binary

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,53 +1,54 @@
-#include "search_algos.h"
+#include"search_algos.h"
 
 /**
- * advanced_binary_helper - searches for a value in a sorted array of integers
- * @array: array of ints
- * @l: left index
- * @r: right index
- * @value: int value we are looking for
- * Return: Always EXIT_SUCCESS
- */
-int advanced_binary_helper(int *array, int l, int r, int value)
+**advanced_binary_recursive-Searchesrecursivelyforavalueinasorted
+**arrayofintegersusingbinarysearch.
+**@array:Apointertothefirstelementofthe[sub]arraytosearch.
+**@left:Thestartingindexofthe[sub]arraytosearch.
+**@right:Theendingindexofthe[sub]arraytosearch.
+**@value:Thevaluetosearchfor.
+**
+**Return:Ifthevalueisnotpresent,-1.
+**else,theindexwherethevalueislocated.
+**
+**Description:Printsthe[sub]arraybeingsearchedaftereachchange.
+**/
+intadvanced_binary_recursive(int*array,size_tleft,size_tright,intvalue)
 {
-	int i, m;
+		size_ti;
 
-	if (l > r || l < 0)
-		return (-1);
+			if(right<left)
+						return(-1);
 
-	printf("Searching in array:");
-	for (i = l; i <= r; i++)
-	{
-		if (i == l)
-			printf(" %d", array[i]);
-		else
-			printf(", %d", array[i]);
-	}
-	printf("\n");
+				printf("Searchinginarray:");
+					for(i=left;i<right;i++)
+								printf("%d,",array[i]);
+						printf("%d\n",array[i]);
 
-	m = (l + r) / 2;
-	if (array[m] == value)
-		if (array[m - 1] != value)
-			return (m);
-		else
-			return (advanced_binary_helper(array, l--, m, value));
-	else if (array[m] < value)
-		return (advanced_binary_helper(array, m + 1, r, value));
-	else
-		return (advanced_binary_helper(array, l, m - 1, value));
+							i=left+(right-left)/2;
+								if(array[i]==value&&(i==left||array[i-1]!=value))
+											return(i);
+									if(array[i]>=value)
+												return(advanced_binary_recursive(array,left,i,value));
+										return(advanced_binary_recursive(array,i+1,right,value));
 }
 
 /**
- * advanced_binary - searches for a value in a sorted array of integers
- * @array: array of ints
- * @size: size of array
- * @value: int value we are looking for
- * Return: Always EXIT_SUCCESS
- */
-int advanced_binary(int *array, size_t size, int value)
+**advanced_binary-Searchesforavalueinasortedarray
+**ofintegersusingadvancedbinarysearch.
+**@array:Apointertothefirstelementofthearraytosearch.
+**@size:Thenumberofelementsinthearray.
+**@value:Thevaluetosearchfor.
+**
+**Return:IfthevalueisnotpresentorthearrayisNULL,-1.
+**Otherwise,thefirstindexwherethevalueislocated.
+**
+**Description:Printsthe[sub]arraybeingsearchedaftereachchange.
+**/
+intadvanced_binary(int*array,size_tsize,intvalue)
 {
-	if (array == NULL || size == 0)
-		return (-1);
+		if(array==NULL||size==0)
+					return(-1);
 
-	return (advanced_binary_helper(array, 0, size - 1, value));
+			return(advanced_binary_recursive(array,0,size-1,value));
 }
